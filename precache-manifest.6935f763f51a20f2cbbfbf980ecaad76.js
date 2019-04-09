10a0a3eb66f00987a1a97df7190d146d86440ac4
EdManDev <ed.manigat@gmail.com>
2019-04-09 12:34:19 -0400

    Updates

---
+++ precache-manifest.6935f763f51a20f2cbbfbf980ecaad76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "08e40f3d0a1394efa9ad",
+    "url": "/EdManDev/AppGoahead/static/css/main.77a0adbb.chunk.css"
+  },
+  {
+    "revision": "08e40f3d0a1394efa9ad",
+    "url": "/EdManDev/AppGoahead/static/js/main.2c11acfd.chunk.js"
+  },
+  {
+    "revision": "e71cf51b98cc700b6357",
+    "url": "/EdManDev/AppGoahead/static/js/runtime~main.8876db86.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/EdManDev/AppGoahead/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/EdManDev/AppGoahead/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "37895c8aef530943251688584cbb91f9",
+    "url": "/EdManDev/AppGoahead/index.html"
+  }
+];
